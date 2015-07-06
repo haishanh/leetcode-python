439c57f3e45e886fa2a99aad4438f2b9423b2d0a
carlhan <hanhaishan1989@gmail.com>
2015-07-06 18:42:31 +0800

    insert_interval: minor mod

--- insert_interval.py
+++ insert_interval.py
@@ -7,65 +7,33 @@ class Interval:
         self.start = s
         self.end = e
 
+
 class Solution:
     # @param {Interval[]} intervals
     # @param {Interval} newInterval
     # @return {Interval[]}
     def insert(self, intervals, newInterval):
-        start = newInterval.start
-        end   = newInterval.end
-        new = []
-        tmp_start = ''
-        for i, e in enumerate(intervals):
-            if end < e.start:
-                new.append(newInterval)
-                if tmp_start:
-                    new.append(Interval(tmp_start, end))
-                new.append(e)
-                break
-            else: # end >= e.start
-                if end <= e.end:
-                    if tmp_start:
-                        new.append(tmp_start, e.end)
-                        break
-                    if start < e.start:
-                        new.append(Interval(start, e.end))
-                        break
-                    else:
-                        new.append(e)
-                        break
-                else:
-                    tmp_start = min(e.start, start)
-                    continue
-        for e in intervals[i:]:
-            new.append(e)
-        return new
-
-# TODO
-class Solution2:
-    # @param {Interval[]} intervals
-    # @param {Interval} newInterval
-    # @return {Interval[]}
-    def insert(self, intervals, newInterval):
+        if not intervals:
+            return [newInterval]
         start =  newInterval.start
         end = newInterval.end
         overlap = []
-        # no overlapping case 1
+        # no overlapping case 1 | [[5,6],[7,8]] [2,4]
         if end < intervals[0].start:
             return [newInterval] + intervals
-        # no overlapping case 2
+        # no overlapping case 2 | [[2,3],[5,7]] [9-11]
         if start > intervals[-1].end:
             return intervals + [newInterval]
         for i, e in enumerate(intervals):
-            if (start <= e.start and end >= e.end) or\
-               (start >= e.start and end <= e.end) or\
-               (start <= e.end and end >= e.end) or\
+            if (start <= e.start and end >= e.end) or \
+               (start >= e.start and end <= e.end) or \
+               (start <= e.end and end >= e.end) or \
                (start <= e.start and end>= e.start):
                 overlap.append(i)
         if overlap:
             tmp_inter = Interval(min(intervals[overlap[0]].start, start), max(intervals[overlap[-1]].end, end))
             return intervals[:overlap[0]] + [tmp_inter] + intervals[overlap[-1]+1:]
-        # no overlapping case 3
+        # no overlapping case 3 | [[2,4],[10,20]] [5,7]
         else:
             for i, e in enumerate(intervals):
                 if end < e.start:
@@ -74,7 +42,7 @@ class Solution2:
 
 
 def test():
-    s = Solution2()
+    s = Solution()
     l = [[1, 3], [6, 9]]
     intervals = []
     for i in l:
@@ -85,6 +53,13 @@ def test():
     new_inter = Interval()
     new_inter.start = 2
     new_inter.end = 5
+    res = s.insert(intervals, new_inter)
+    for i in res:
+        print(i.start, i.end)
+    print('-' * 20)
+    intervals = []
+    new_inter.start = 5
+    new_inter.end = 7
     res = s.insert(intervals, new_inter)
     for i in res:
         print(i.start, i.end)
